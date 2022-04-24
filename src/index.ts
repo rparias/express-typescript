52e4073d45cfad4260b7ae0429c766d2c52d6764
Ronald Arias <ariasron@hotmail.com>
2022-04-24 15:04:57 -0500

    installs express and adds initial configuration

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json()) // middleware to return req.body as json
+
+const PORT = 3001
+
+app.get('/ping', (_req, res) => {
+  console.log('someone has pinged here')
+  res.send('pong')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
